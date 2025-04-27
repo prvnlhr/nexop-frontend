0894209ea9abf5d969d11ee2721108a345c739be
Praveen Lohar <praveenlohar.in@gmail.com>
2025-04-28 02:28:07 +0530

    temp fix

--- src/app/(admin)/admin/inventory/categories/add/page.tsx
+++ src/app/(admin)/admin/inventory/categories/add/page.tsx
@@ -1,11 +1,12 @@
-import CategoryAddForm from "@/components/Admin/Pages/Category/CategoryAddForm";
-import { getCategories } from "@/lib/services/admin/categoryServices";
-import { Category } from "@/types/categoryTypes";
+// import CategoryAddForm from "@/components/Admin/Pages/Category/CategoryAddForm";
+// import { getCategories } from "@/lib/services/admin/categoryServices";
+// import { Category } from "@/types/categoryTypes";
 import React from "react";
 
 const page = async () => {
-  const categories: Category[] = await getCategories();
-  return <CategoryAddForm categories={categories} />;
+  // const categories: Category[] = await getCategories();
+  // return <CategoryAddForm categories={categories} />;
+  return <p></p>;
 };
 
 export default page;
