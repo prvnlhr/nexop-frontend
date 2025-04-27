331a168918b84298f3d81f17f3aada96feda2e6a
Praveen Lohar <praveenlohar.in@gmail.com>
2025-04-28 02:22:22 +0530

    temp fix

--- src/app/(admin)/admin/inventory/attributes/add/page.tsx
+++ src/app/(admin)/admin/inventory/attributes/add/page.tsx
@@ -1,12 +1,13 @@
 export const dynamic = "force-dynamic";
-import AttributeAddForm from "@/components/Admin/Pages/Attribute/AttributeAddForm";
-import { getCategories } from "@/lib/services/admin/categoryServices";
-import { Category } from "@/types/categoryTypes";
+// import AttributeAddForm from "@/components/Admin/Pages/Attribute/AttributeAddForm";
+// import { getCategories } from "@/lib/services/admin/categoryServices";
+// import { Category } from "@/types/categoryTypes";
 import React from "react";
 
 const page = async () => {
-  const categories: Category[] = await getCategories();
-  return <AttributeAddForm categories={categories} isEditMode={false} />;
+  // const categories: Category[] = await getCategories();
+  // return <AttributeAddForm categories={categories} isEditMode={false} />;
+  return <p></p>;
 };
 
 export default page;
