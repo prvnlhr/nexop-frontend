74ebd02523aa1b1822d3d79fd8c010622b20e548
Praveen Lohar <praveenlohar.in@gmail.com>
2025-04-28 02:31:14 +0530

    temp fix

--- src/app/(admin)/admin/inventory/categories/page.tsx
+++ src/app/(admin)/admin/inventory/categories/page.tsx
@@ -1,11 +1,12 @@
-import CategoryPage from "@/components/Admin/Pages/Category/CategoryPage";
-import { getCategories } from "@/lib/services/admin/categoryServices";
-import { Category } from "@/types/categoryTypes";
+// import CategoryPage from "@/components/Admin/Pages/Category/CategoryPage";
+// import { getCategories } from "@/lib/services/admin/categoryServices";
+// import { Category } from "@/types/categoryTypes";
 import React from "react";
 
 const page = async () => {
-  const categories: Category[] = await getCategories();
-  return <CategoryPage categories={categories} />;
+  // const categories: Category[] = await getCategories();
+  // return <CategoryPage categories={categories} />;
+  return <p></p>;
 };
 
 export default page;
