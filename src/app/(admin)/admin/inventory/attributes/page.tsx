ec2f7439631f19de79f3377d06e5aa80048df3a7
Praveen Lohar <praveenlohar.in@gmail.com>
2025-04-28 02:25:11 +0530

    temp fix

--- src/app/(admin)/admin/inventory/attributes/page.tsx
+++ src/app/(admin)/admin/inventory/attributes/page.tsx
@@ -1,11 +1,13 @@
-import AttributePage from "@/components/Admin/Pages/Attribute/AttributePage";
-import { getAllAttributes } from "@/lib/services/admin/attributeServices";
-import { AttributeItem } from "@/types/attributeTypes";
+export const dynamic = "force-dynamic";
+// import AttributePage from "@/components/Admin/Pages/Attribute/AttributePage";
+// import { getAllAttributes } from "@/lib/services/admin/attributeServices";
+// import { AttributeItem } from "@/types/attributeTypes";
 import React from "react";
 
 const page = async () => {
-  const attributes: AttributeItem[] = await getAllAttributes();
-  return <AttributePage attributes={attributes} />;
+  // const attributes: AttributeItem[] = await getAllAttributes();
+  // return <AttributePage attributes={attributes} />;
+  return <p></p>;
 };
 
 export default page;
